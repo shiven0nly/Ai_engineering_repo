cdcaa893fd8c467002c83e23656c9e954039ddfb
Shiven Sharma <shiven676@gmail.com>
2026-07-14 21:54:59 +0530

    feat: Implemented Pydantic Schemas

---
+++ mini_py_projects/resume_parser/models.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel
+from typing import List
+
+class Education(BaseModel):
+    college: str
+    degree: str
+    cgpa: float | None = None
+    percentage_12: float | None = None
+    
+class Experience(BaseModel):
+    total_years: float | None = None
+    company: str | None = None
+    role: str | None = None
+    
+class Projects(BaseModel):
+    title: str
+    technologies_used: List[str] | None = None
+    
+    
+    
+class Resume(BaseModel):
+    job_title: str | None = None
+    name: str
+    email: str
+    phone: str
+    skills: List[str]
+    projects: List[Projects]
+    education: List[Education]
+    experience: List[Experience]
+    certifications: List[str]
+    languages: List[str] | None = None
+    summary: str
+    
