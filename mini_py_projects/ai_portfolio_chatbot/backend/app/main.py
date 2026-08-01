4ef9018f5532c4aa76b2de36369706b97ea85439
Shiven Sharma <shiven676@gmail.com>
2026-08-01 15:18:03 +0530

    fix: updatin cors code for frontend to backend connection

--- mini_py_projects/ai_portfolio_chatbot/backend/app/main.py
+++ mini_py_projects/ai_portfolio_chatbot/backend/app/main.py
@@ -21,7 +21,7 @@ app = FastAPI(
 # Enable CORS for React Frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["https://shivenaiportfoliobot.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
