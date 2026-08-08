31c6cb20dbe392f2f11408172589952f053e63bb
Shiven Sharma <shiven676@gmail.com>
2026-08-09 00:08:05 +0530

    learn: python basics

--- learning_python/practicing/set9.py
+++ learning_python/practicing/set9.py
@@ -31,3 +31,75 @@ print(f"your username become: {username}")
 list = [1,3,4,5,1,5,6,3]
 new_list = set(list)
 print(new_list)
+
+# write a program to print the star pattern:
+"""
+*
+**
+***
+****
+*****
+"""
+star = "*"
+for i in range(1,6):
+    print(f"{star*i}")
+print("\n\n")
+# prin the pattern:
+"""
+*    # ek star 
+**   # do star
+***  # teen star
+**   # do star
+*    # ek star
+"""
+for i in range(1,4):
+    print(f"{star*i}")
+for i in range(3,0,-1):
+    print(f"{star*i}")
+print("\n\n")
+
+# print this pattern:
+"""
+        * 
+      * * * 
+    * * * * * 
+  * * * * * * * 
+* * * * * * * * *   
+
+"""
+space = " "
+for i in range(1,6):
+    stars = " ".join([star] * (2*i - 1))
+    print(f"{space*(5-i)}{stars}")
+    
+# print this pattern:
+"""
+
+1 
+1 2 1 
+1 2 3 2 1 
+1 2 3 4 3 2 1 
+1 2 3 4 5 4 3 2 1 """
+
+for i in range(1,6):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    for j in range(i - 1, 0, -1):
+        print(j, end=" ")
+    print()
+print("\n\n")
+
+# print the following program:
+"""
+1
+2 3
+4 5 6
+7 8 9 10
+"""
+
+num = 1
+for i in range(1,5):
+    for j in range(1,i+1):
+        print(num,end=" ")
+        num += 1
+    print()
